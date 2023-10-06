7e9706a9d03fbad6b4a350aedc1334aebdb465f7
harshadakahane <harshadakahane281@gmail.com>
2023-10-06 16:39:10 +0530

    Print array elements

---
+++ Arrays/Array1.java
@@ -0,0 +1,14 @@
+class Arraydemo
+{
+	public static void main(String[] args)
+	{
+		int[] arr = new int[]{5,6,3,5,2,4};
+		int n =6;
+
+		for(int i=0; i<n; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
+
