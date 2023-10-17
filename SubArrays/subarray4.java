9277a9746ac935746233f7eaa05780067094f34a
harshadakahane <harshadakahane281@gmail.com>
2023-10-17 12:42:34 +0530

    print the sum of array of every single subarray

---
+++ SubArrays/subarray4.java
@@ -0,0 +1,25 @@
+class subarray
+{
+	public static void main(String[] args)
+	{
+		int[] arr = new int[]{2,4,1,3};
+		
+
+		for(int i=0;i<arr.length; i++)
+		{
+			for(int j=i; j<arr.length; j++)
+			{
+				int sum=0;
+				for(int k=i; k<=j; k++)
+				{
+					sum = sum + arr[k];
+				}
+				 System.out.println(sum);
+		
+			}
+			
+		}
+	        
+	}
+}
+
