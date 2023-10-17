2d3378d5e9f5fec1bf90942b2d0f811aa8acc3e7
harshadakahane <harshadakahane281@gmail.com>
2023-10-17 12:58:49 +0530

    print the sum of every single subarray without using any extra space

---
+++ SubArrays/subarray7.java
@@ -0,0 +1,22 @@
+class demo
+{
+	public static void main(String[] args)
+	{
+		int[] arr = new int[]{2,4,1,3};
+		for(int i=0; i<arr.length; i++)
+		{
+			int sum =0;
+			for(int j =1; j<arr.length; j++)
+			{
+				sum = sum + arr[j];
+			
+				System.out.println(sum);
+			}
+		
+		
+		}
+	
+	}
+}
+
+
